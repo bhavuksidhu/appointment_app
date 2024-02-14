13a4a3b5b86ec8788e7a77f75f56d93f53dd29e2
Bhavuk Sidhu <bhavuksidhu@gmail.com>
2024-02-14 16:22:38 +0530

    fix update api

--- app/serializers/member_serializer.rb
+++ app/serializers/member_serializer.rb
@@ -3,7 +3,7 @@ class MemberSerializer
   attributes :id, :first_name, :last_name, :age, :gender, :role
 
   attributes :avatar do |object|
-    host = (Rails.env.development? || Rails.env.test?) ? 'http://localhost:3000' : ENV['APP_URL']
+    host = (Rails.env.development? || Rails.env.test?) ? 'http://localhost:3000' : 'https://fierce-castle-51356-3f74a812b9eb.herokuapp.com'
     host + Rails.application.routes.url_helpers.rails_blob_path(object.avatar, only_path: true) if object.avatar.attached?
   end
 end
